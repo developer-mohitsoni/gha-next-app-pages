360783f6d7d7bfb898d2d4dbecedf19098ad56a2
Mohit Soni <developer_mohitsoni@outlook.com>
2025-06-12 14:28:22 +0530

    Add GitHub Actions workflow for deployment and configure Next.js settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  output: 'export', // Required for static export in Next.js 13+
+  assetPrefix: isProd ? '/gha-next-app-pages/' : '',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+};
 
 export default nextConfig;
